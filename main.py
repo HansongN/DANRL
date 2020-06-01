80bc6233f481e08b6af8b49606ca8760904743d8
HansongN <48521208+HansongN@users.noreply.github.com>
2020-06-01 10:49:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,26 +5,26 @@
 from utils import load_any_obj_pkl, save_any_obj_pkl
 from DANRL import DANRL
 
-# ratio_most_affected_nodes = 0.2
-# num_walks = 20
-# walk_length = 80
-# window_size = 10
-# embedding_dimensional = 128
-# num_negative = 10
-#
-# G_dynamic = load_any_obj_pkl("graph_data/collaborate_network(2G)/collaborate_network_2007_2016.pkl")
-#
-#
-# model = DANRL(G_dynamic=G_dynamic,
-#                       limit=ratio_most_affected_nodes,
-#                       local_global=1,
-#                       num_walks=num_walks,
-#                       walk_length=walk_length,
-#                       window=window_size,
-#                       emb_dim=embedding_dimensional,
-#                       n_negative=num_negative)
-# emb_dicts = model.train()
-# save_any_obj_pkl(obj=emb_dicts, path="output/collaborate_network(2G)/DANRL/collaborate_network_2007_2016_embs.pkl")
+ratio_most_affected_nodes = 0.2
+num_walks = 20
+walk_length = 80
+window_size = 10
+embedding_dimensional = 128
+num_negative = 10
+
+G_dynamic = load_any_obj_pkl("graph_data/collaborate_network(2G)/collaborate_network_2007_2016.pkl")
+
+
+model = DANRL(G_dynamic=G_dynamic,
+                      limit=ratio_most_affected_nodes,
+                      local_global=1,
+                      num_walks=num_walks,
+                      walk_length=walk_length,
+                      window=window_size,
+                      emb_dim=embedding_dimensional,
+                      n_negative=num_negative)
+emb_dicts = model.train()
+save_any_obj_pkl(obj=emb_dicts, path="output/collaborate_network(2G)/DANRL/collaborate_network_2007_2016_embs.pkl")
 
 # import os
 # import numpy as np
